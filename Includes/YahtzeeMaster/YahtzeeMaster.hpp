722bd0b27dc2ab6bb0e3ce979304bba7ddd52c38
Chris Ohk <utilForever@gmail.com>
2020-08-12 13:52:34 +0900

    feat(add-ci): Add header generator script

---
+++ Includes/YahtzeeMaster/YahtzeeMaster.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Chris Ohk
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef YAHTZEE_MASTER_HPP
+#define YAHTZEE_MASTER_HPP
+
+#include <YahtzeeMaster/Test.hpp>
+#include <YahtzeeMaster/YahtzeeMaster.hpp>
+
+#endif  // YAHTZEE_MASTER_HPP
