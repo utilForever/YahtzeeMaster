86a5863c5056493d8b9208be2e314c3da86bcb44
Chris Ohk <utilforever@gmail.com>
2020-08-26 01:03:00 +0900

    feat(core): Create struct 'GameState'
    - This struct stores the information of the game state of all players

---
+++ Includes/YahtzeeMaster/Games/GameState.hpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2020 Chris Ohk
+
+// I am making my contributions/submissions to this project solely in our
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef YAHTZEE_MASTER_GAME_STATE_HPP
+#define YAHTZEE_MASTER_GAME_STATE_HPP
+
+#include <YahtzeeMaster/Models/Player.hpp>
+
+#include <vector>
+
+namespace YahtzeeMaster
+{
+//!
+//! \brief GameState struct.
+//!
+//! This struct stores the information of the game state of all players.
+//!
+struct GameState
+{
+    std::vector<Player> players;
+};
+}  // namespace YahtzeeMaster
+
+#endif
