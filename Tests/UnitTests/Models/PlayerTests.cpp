26284357c63629fdefd0ca333952eb89af72d495
Chris Ohk <utilforever@gmail.com>
2020-08-25 23:58:52 +0900

    test: Add unit test code for class 'Player'

---
+++ Tests/UnitTests/Models/PlayerTests.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2020 Chris Ohk
+
+// I am making my contributions/submissions to this project solely in our
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "doctest_proxy.hpp"
+
+#include <YahtzeeMaster/Models/Player.hpp>
+
+using namespace YahtzeeMaster;
+
+TEST_CASE("[Player] - RollDices")
+{
+    Player player;
+
+    player.RollDices();
+
+    std::array<int, 5> values = player.GetDiceValues();
+    for (auto& value : values)
+    {
+        CHECK((value >= 1 && value <= 6));
+    }
+}
