1b7cedad5cb7fdc14e176b4287d48937e67bb7f4
Chris Ohk <utilForever@gmail.com>
2020-08-27 15:28:36 +0900

    test: Add code to check method 'RollDices()' and re-roll condition

--- Tests/UnitTests/Games/GameTests.cpp
+++ Tests/UnitTests/Games/GameTests.cpp
@@ -18,9 +18,30 @@ TEST_CASE("[Game] - GetGameState")
     state.players.emplace_back(Player{});
     state.players[0].RollDices();
 
-    std::array<int, NUM_DICES> values = state.players[0].GetDiceValues();
-    for (auto& value : values)
+    std::array<int, NUM_DICES> values1 = state.players[0].GetDiceValues();
+    for (auto& value : values1)
     {
         CHECK((value >= 1 && value <= 6));
+    }
+
+    state.players[0].RollDices({ 0, 1, 3 });
+
+    std::array<int, NUM_DICES> values2 = state.players[0].GetDiceValues();
+    CHECK_EQ(values1[2], values2[2]);
+    CHECK_EQ(values1[4], values2[4]);
+
+    state.players[0].RollDices({ 2, 3 });
+
+    std::array<int, NUM_DICES> values3 = state.players[0].GetDiceValues();
+    CHECK_EQ(values2[0], values3[0]);
+    CHECK_EQ(values2[1], values3[1]);
+    CHECK_EQ(values2[4], values3[4]);
+
+    state.players[0].RollDices();
+
+    std::array<int, NUM_DICES> values4 = state.players[0].GetDiceValues();
+    for (std::size_t i = 0; i < static_cast<std::size_t>(NUM_DICES); ++i)
+    {
+        CHECK_EQ(values3[i], values4[i]);
     }
 }
