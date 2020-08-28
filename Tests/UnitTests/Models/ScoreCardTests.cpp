83560a8a299faba6c28f8856c1fed96d9ccc1b03
Chris Ohk <utilforever@gmail.com>
2020-08-28 23:59:13 +0900

    test: Add unit test code to check method 'IsFourOfAKind()'

--- Tests/UnitTests/Models/ScoreCardTests.cpp
+++ Tests/UnitTests/Models/ScoreCardTests.cpp
@@ -18,3 +18,12 @@ TEST_CASE("[ScoreCard] - ThreeOfAKind")
     CHECK_EQ(ScoreCard::IsThreeOfAKind(diceValues1), false);
     CHECK_EQ(ScoreCard::IsThreeOfAKind(diceValues2), true);
 }
+
+TEST_CASE("[ScoreCard] - FourOfAKind")
+{
+    const std::array<int, NUM_DICES> diceValues1{ 1, 4, 4, 4, 5 };
+    const std::array<int, NUM_DICES> diceValues2{ 1, 4, 4, 4, 4 };
+
+    CHECK_EQ(ScoreCard::IsFourOfAKind(diceValues1), false);
+    CHECK_EQ(ScoreCard::IsFourOfAKind(diceValues2), true);
+}
