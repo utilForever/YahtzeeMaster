df2da551a9aef2819e80733b65e91bbe800d1849
Chris Ohk <utilforever@gmail.com>
2020-08-28 23:51:58 +0900

    test: Add unit test code to check method 'IsThreeOfAKind()'

---
+++ Tests/UnitTests/Models/ScoreCardTests.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2020 Chris Ohk
+
+// I am making my contributions/submissions to this project solely in our
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "doctest_proxy.hpp"
+
+#include <YahtzeeMaster/Models/ScoreCard.hpp>
+
+using namespace YahtzeeMaster;
+
+TEST_CASE("[ScoreCard] - ThreeOfAKind")
+{
+    const std::array<int, NUM_DICES> diceValues1{ 1, 2, 4, 4, 5 };
+    const std::array<int, NUM_DICES> diceValues2{ 1, 4, 4, 4, 5 };
+
+    CHECK_EQ(ScoreCard::IsThreeOfAKind(diceValues1), false);
+    CHECK_EQ(ScoreCard::IsThreeOfAKind(diceValues2), true);
+}
