cf31aecf61c316408dbbe7878fa8815b7090fad7
Chris Ohk <utilforever@gmail.com>
2020-08-28 23:59:27 +0900

    feat(core): Add enum 'Category'
    - An enumerator for identifying the category

--- Includes/YahtzeeMaster/Enums/GameEnums.hpp
+++ Includes/YahtzeeMaster/Enums/GameEnums.hpp
@@ -16,6 +16,24 @@ enum class Step
     PLAY_ROUND,
     COMPLETE
 };
+
+//! \brief An enumerator for identifying the category.
+enum Category
+{
+    ACES,
+    TWOS,
+    THREES,
+    FOURS,
+    FIVES,
+    SIXES,
+    THREE_OF_A_KIND,
+    FOUR_OF_A_KIND,
+    FULL_HOUSE,
+    SMALL_STRAIGHT,
+    LARGE_STRAIGHT,
+    YAHTZEE,
+    CHANCE
+};
 }  // namespace YahtzeeMaster
 
 #endif
