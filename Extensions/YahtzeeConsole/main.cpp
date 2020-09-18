a836069dfe63cf2bd12f834eb5cf4b2df73ba57b
Chris Ohk <utilForever@gmail.com>
2020-09-18 13:51:49 +0900

    feat(console): Create project 'YahtzeeConsole'

---
+++ Extensions/YahtzeeConsole/main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Chris Ohk
+
+// I am making my contributions/submissions to this project solely in our
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+int main()
+{
+    return 0;
+}
