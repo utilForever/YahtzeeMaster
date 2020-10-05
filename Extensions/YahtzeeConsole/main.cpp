36f0b6cc9347590228fc9eb8e4932b37ea29b56e
Chris Ohk <utilForever@gmail.com>
2020-10-05 14:08:52 +0900

    feat(console): Add code to process basic CLI

--- Extensions/YahtzeeConsole/main.cpp
+++ Extensions/YahtzeeConsole/main.cpp
@@ -4,7 +4,32 @@
 // personal capacity and am not conveying any rights to any intellectual
 // property of any third parties.
 
-int main()
+#include <lyra/lyra.hpp>
+
+int main(int argc, char* argv[])
 {
-    return 0;
+    int mode = 1;
+    bool showHelp = false;
+
+    // Process CLI
+    const auto cli =
+        lyra::cli() | lyra::help(showHelp) |
+        lyra::opt(mode, "mode")["-m"]["--mode"](
+            "The game mode. (1 - Player vs Computer, 2 - Computer vs Computer)");
+    const auto result = cli.parse({ argc, argv });
+
+    if (!result)
+    {
+        std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+        std::cerr << cli << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (showHelp)
+    {
+        std::cout << cli << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
 }
